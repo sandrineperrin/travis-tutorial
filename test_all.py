5653af7678343ae5747f95a20ae3d74f20dbe47e
Sandrine Perrin <sandrine.perrin@france-bioinformatique.fr>
2017-05-31 12:16:44 +0200

    UPDATE travis.yml : include numpy.

--- test_all.py
+++ test_all.py
@@ -1,3 +1,10 @@
+import numpy as np
+
+def test_numpy_partition():
+    in_ar = np.array([3,2,-1,10])
+    parti = np.partition(in_ar,1)
+    assert parti[1] == 2 
+
 def test_division():
     assert 3./2 == 1.5
 
